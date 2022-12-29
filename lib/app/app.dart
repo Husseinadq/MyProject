cf87642fec8a91d1bb1a0a9ea26effa8420b70eb
husseinadq <husseinadq2@gmail.com>
2022-12-29 10:34:58 +0300

    create strings manager

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test01/presentation/theme_manager.dart';
+import 'package:test01/presentation/resources/theme_manager.dart';
 class MyApp extends StatefulWidget {
 
   MyApp._internal();
